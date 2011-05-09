0fdc310ea6636c4f17145ef010f9056fd0f11ccf
Ghislain Roquier <ghislain.roquier@insa-rennes.fr>
2011-05-09 16:08:09 +0000

    remove hacky properties of component. Todo: provides abstractions for defining operator, medium, etc. Support of hierarchy is also required.
    git-svn-id: http://svn.code.sf.net/p/dftools/svn/trunk@21 241f8b49-2e81-4b1e-ac05-70ce8349dc88

--- eclipse/plugins/net.sf.dftools.architecture/src/net/sf/dftools/architecture/component/Component.java
+++ eclipse/plugins/net.sf.dftools.architecture/src/net/sf/dftools/architecture/component/Component.java
@@ -7,11 +7,8 @@
  */
 package net.sf.dftools.architecture.component;
 
-import java.util.Arrays;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.Map;
-import java.util.Set;
 
 import net.sf.dftools.architecture.VLNV;
 
@@ -23,17 +20,6 @@ import net.sf.dftools.architecture.VLNV;
  */
 
 public class Component {
-
-	private static final Set<String> processors = new HashSet<String>(
-			Arrays.asList("x86", "PowerPC"));
-	private static final Set<String> fpgas = new HashSet<String>(
-			Arrays.asList("SP605"));
-	private static final Set<String> buses = new HashSet<String>(Arrays.asList(
-			"Ethernet", "RS232", "SharedMemory"));
-
-	public enum Type {
-		PROCESSOR, FPGA, BUS
-	};
 
 	private String clasz;
 
@@ -49,45 +35,15 @@ public class Component {
 		return interfaces.get(name);
 	}
 
-	private Type type;
-
 	public Component(String clasz, VLNV vlnv,
 			Map<String, BusInterface> interfaces) {
 		this.clasz = clasz;
 		this.vlnv = vlnv;
 		this.interfaces = interfaces;
-
-		if (processors.contains(clasz)) {
-			type = Type.PROCESSOR;
-		} else if (fpgas.contains(clasz)) {
-			type = Type.FPGA;
-		} else if (buses.contains(clasz)) {
-			type = Type.BUS;
-		}
 	}
 
 	public VLNV getVlnv() {
 		return vlnv;
-	}
-
-	public void setType(Type type) {
-		this.type = type;
-	}
-
-	public Type getType() {
-		return type;
-	}
-
-	public boolean isProcessor() {
-		return type == Type.PROCESSOR;
-	}
-
-	public boolean isFPGA() {
-		return type == Type.FPGA;
-	}
-
-	public boolean isBus() {
-		return type == Type.BUS;
 	}
 
 	public String getClasz() {
