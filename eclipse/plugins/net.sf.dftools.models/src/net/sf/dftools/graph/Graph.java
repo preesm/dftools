08dace6ed9d6f0c46c815aad4baabfca9ec02ef2
Matthieu Wipliez <matthieu.wipliez@insa-rennes.fr>
2012-02-15 17:02:34 +0000

    (javadoc) small fix in javadoc
    git-svn-id: http://svn.code.sf.net/p/dftools/svn/trunk@137 241f8b49-2e81-4b1e-ac05-70ce8349dc88

--- eclipse/plugins/net.sf.dftools.models/src/net/sf/dftools/graph/Graph.java
+++ eclipse/plugins/net.sf.dftools.models/src/net/sf/dftools/graph/Graph.java
@@ -11,7 +11,9 @@ import org.eclipse.emf.common.util.EList;
 import org.eclipse.emf.ecore.EObject;
 
 /**
- * This class defines a graph as a list of vertices and edges.
+ * This class defines a graph as a list of vertices and edges. The vertices are
+ * not contained in this graph; containment is considered to be the
+ * responsibility of clients of this class (including subclasses).
  * 
  * @model
  */
@@ -26,9 +28,7 @@ public interface Graph extends EObject {
 	EList<Edge> getEdges();
 
 	/**
-	 * Returns the list of this graph's vertices. The vertices are not contained
-	 * in this graph; containment is considered to be the responsibility of
-	 * clients of this class (including subclasses).
+	 * Returns the list of this graph's vertices.
 	 * 
 	 * @return the list of this graph's vertices
 	 * @model
