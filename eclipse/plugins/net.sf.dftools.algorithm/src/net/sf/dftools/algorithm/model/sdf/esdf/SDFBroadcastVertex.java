e6052c429ac63242b615a0dca336d1300c6deac7
Jonathan Piat <jpiat@laas.fr>
2012-05-11 16:05:58 +0000

    small changes to addEdge and removeEdge
    git-svn-id: http://svn.code.sf.net/p/dftools/svn/trunk@223 241f8b49-2e81-4b1e-ac05-70ce8349dc88

--- eclipse/plugins/net.sf.dftools.algorithm/src/net/sf/dftools/algorithm/model/sdf/esdf/SDFBroadcastVertex.java
+++ eclipse/plugins/net.sf.dftools.algorithm/src/net/sf/dftools/algorithm/model/sdf/esdf/SDFBroadcastVertex.java
@@ -1,21 +1,31 @@
 package net.sf.dftools.algorithm.model.sdf.esdf;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import net.sf.dftools.algorithm.model.AbstractEdge;
+import net.sf.dftools.algorithm.model.PropertySource;
 import net.sf.dftools.algorithm.model.parameters.InvalidExpressionException;
 import net.sf.dftools.algorithm.model.sdf.SDFAbstractVertex;
+import net.sf.dftools.algorithm.model.sdf.SDFEdge;
 
 /**
  * Special vertex that supports broadcast
+ * 
  * @author jpiat
- *
+ * 
  */
-public class SDFBroadcastVertex extends SDFAbstractVertex{
+public class SDFBroadcastVertex extends SDFAbstractVertex {
 
 	/**
 	 * Kind of node
 	 */
-	public static final String BROADCAST ="Broadcast"; 
-	
+	public static final String BROADCAST = "Broadcast";
+
+	/**
+	 * String to access the property edges order
+	 */
+	public static final String EDGES_ORDER = "edges_order";
 
 	/**
 	 * Creates a new SDFInterfaceVertex with the default direction (SINK)
@@ -36,21 +46,55 @@ public class SDFBroadcastVertex extends SDFAbstractVertex{
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		return copy ;
+		return copy;
+	}
+
+	protected void addConnection(SDFEdge newEdge) {
+		getConnections().put(newEdge, getConnections().size());
+	}
+
+	protected void removeConnection(SDFEdge newEdge) {
+		getConnections().remove(newEdge);
 	}
 
 	@SuppressWarnings("rawtypes")
 	@Override
 	public void connectionAdded(AbstractEdge e) {
-		// Nothing to do for the moment
+		addConnection((SDFEdge) e);
 	}
 
 	@SuppressWarnings("rawtypes")
 	@Override
 	public void connectionRemoved(AbstractEdge e) {
-		// Nothing to do for the moment
-		
+		removeConnection((SDFEdge) e);
 	}
 
+	/**
+	 * Gives the edge connection index
+	 * 
+	 * @param edge
+	 *            The edge to get the connection index
+	 * @return The connection index of the edge
+	 */
+	public Integer getEdgeIndex(SDFEdge edge) {
+		return getConnections().get(edge);
+	}
+
+	@SuppressWarnings("unchecked")
+	protected Map<SDFEdge, Integer> getConnections() {
+		Map<SDFEdge, Integer> connections;
+		if ((connections = (Map<SDFEdge, Integer>) this.getPropertyBean()
+				.getValue(EDGES_ORDER)) == null) {
+			connections = new HashMap<SDFEdge, Integer>();
+			this.getPropertyBean().setValue(EDGES_ORDER, connections);
+		}
+		return connections;
+	}
+
+	public void copyProperties(PropertySource props) {
+		super.copyProperties(props);
+		Map<SDFEdge, Integer> connections = new HashMap<SDFEdge, Integer>();
+		this.getPropertyBean().setValue(EDGES_ORDER, connections);
+	}
 
 }
--- eclipse/plugins/net.sf.dftools.algorithm/src/net/sf/dftools/algorithm/model/sdf/esdf/SDFRoundBufferVertex.java
+++ eclipse/plugins/net.sf.dftools.algorithm/src/net/sf/dftools/algorithm/model/sdf/esdf/SDFRoundBufferVertex.java
@@ -1,33 +1,31 @@
 package net.sf.dftools.algorithm.model.sdf.esdf;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import net.sf.dftools.algorithm.model.AbstractEdge;
-import net.sf.dftools.algorithm.model.PropertySource;
 import net.sf.dftools.algorithm.model.parameters.InvalidExpressionException;
 import net.sf.dftools.algorithm.model.sdf.SDFAbstractVertex;
 import net.sf.dftools.algorithm.model.sdf.SDFEdge;
 
 /**
- * ROund buffer vertex 
+ * ROund buffer vertex
+ * 
  * @author jpiat
- *
+ * 
  */
-public class SDFRoundBufferVertex extends SDFBroadcastVertex{	
+public class SDFRoundBufferVertex extends SDFBroadcastVertex {
 
 	/**
 	 * String to access the property edges order
 	 */
-	public static final String EDGES_ORDER ="edges_order"; 
-	
+	public static final String EDGES_ORDER = "edges_order";
+
 	/**
 	 * Kind of node
 	 */
-	public static final String ROUND_BUFFER ="RoundBuffer";
-	
+	public static final String ROUND_BUFFER = "RoundBuffer";
+
 	/**
 	 * Creates a new SDFInterfaceVertex with the default direction (SINK)
 	 */
@@ -35,74 +33,49 @@ public class SDFRoundBufferVertex extends SDFBroadcastVertex{
 		super();
 		setNbRepeat(1);
 	}
-	
-	private void addConnection(SDFEdge newEdge){
-		getConnections().put(newEdge, getConnections().size());
-	}
-	
-	private void removeConnection(SDFEdge newEdge){
-		getConnections().remove(newEdge);
-	}
-	
+
 	/**
 	 * Gives the edge connection index
-	 * @param edge The edge to get the connection index
-	 * @return  The connection index of the edge
+	 * 
+	 * @param edge
+	 *            The edge to get the connection index
+	 * @return The connection index of the edge
 	 */
-	public Integer getEdgeIndex(SDFEdge edge){
+	public Integer getEdgeIndex(SDFEdge edge) {
 		return getConnections().get(edge);
 	}
-	
-	@SuppressWarnings("unchecked" )
-	private Map<SDFEdge, Integer> getConnections(){
-		Map<SDFEdge, Integer> connections ;
-		if((connections = (Map<SDFEdge, Integer> )this.getPropertyBean().getValue(EDGES_ORDER)) == null){
-			connections = new HashMap<SDFEdge, Integer>();
-			this.getPropertyBean().setValue(EDGES_ORDER, connections);
-		}
-		return connections;
-	}
-	
+
 	/**
 	 * @return The incoming connection of this for in an ordered list
 	 */
-	public List<SDFEdge> getIncomingConnections(){
+	public List<SDFEdge> getIncomingConnections() {
 		List<SDFEdge> edges = new ArrayList<SDFEdge>(getConnections().size());
-		for(SDFEdge edge : getConnections().keySet()){
-			edges.add(getConnections().get(edge),edge);
+		for (SDFEdge edge : getConnections().keySet()) {
+			edges.add(getConnections().get(edge), edge);
 		}
 		return edges;
 	}
-	
-	/** Sets this edge connection index 
-	 * @param edge The edge connected to the vertex
-	 * @param index The index in the connections
+
+	/**
+	 * Sets this edge connection index
+	 * 
+	 * @param edge
+	 *            The edge connected to the vertex
+	 * @param index
+	 *            The index in the connections
 	 */
-	public void setConnectionIndex(SDFEdge edge, int index){
-		Map<SDFEdge, Integer> connections  = getConnections();
+	public void setConnectionIndex(SDFEdge edge, int index) {
+		Map<SDFEdge, Integer> connections = getConnections();
 		SDFEdge connectionToRemove = null;
-		for(SDFEdge existingConnections : connections.keySet()){
-			if(connections.get(existingConnections) == index){
-				connectionToRemove = existingConnections ;
+		for (SDFEdge existingConnections : connections.keySet()) {
+			if (connections.get(existingConnections) == index) {
+				connectionToRemove = existingConnections;
 			}
 		}
 		connections.remove(connectionToRemove);
 		connections.put(edge, index);
 	}
-	
-	@SuppressWarnings("rawtypes")
-	@Override
-	public void connectionAdded(AbstractEdge e) {
-		addConnection((SDFEdge) e);
-	}
 
-
-	@SuppressWarnings("rawtypes")
-	@Override
-	public void connectionRemoved(AbstractEdge e) {
-		removeConnection((SDFEdge) e);
-	}
-	
 	@Override
 	public SDFAbstractVertex clone() {
 		SDFRoundBufferVertex copy = new SDFRoundBufferVertex();
@@ -113,13 +86,7 @@ public class SDFRoundBufferVertex extends SDFBroadcastVertex{
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		return copy ;
+		return copy;
 	}
-	
-	public void copyProperties(PropertySource props){
-		super.copyProperties(props);
-		Map<SDFEdge, Integer> connections = new HashMap<SDFEdge, Integer>();
-		this.getPropertyBean().setValue(EDGES_ORDER, connections);
-	}
-	
+
 }
