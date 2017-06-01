f851283e5f81035a11c8b151fef53c0c23401863
Antoine Morvan <antoine.morvan@insa-rennes.fr>
2017-06-01 11:08:53 +0200

    [RELENG] Update licensing scripts and config

--- releng/fix_header_copyright_and_authors.sh
+++ releng/fix_header_copyright_and_authors.sh
@@ -1,61 +1,32 @@
 #!/bin/bash
 
+echo ""
+
 #########
-##	
-## 	Automatically replace dates and author list
+##
+##  Automatically replace dates and author list
 ##  in files containing the corresponding patterns
 ##  (see {LOWDATE|UPPDATE|AUTHORS}PATTERN variable
 ##  below). Information is fetched from the git 
 ##  repository.
-#*******************************************************************************
-# Copyright or © or Copr. IETR/INSA - Rennes (%%DATE%%) :
-#
-# %%AUTHORS%%
-#
-# This software is a computer program whose purpose is to [describe
-# functionalities and technical features of your software].
-#
-# This software is governed by the CeCILL  license under French law and
-# abiding by the rules of distribution of free software.  You can  use, 
-# modify and/ or redistribute the software under the terms of the CeCILL
-# license as circulated by CEA, CNRS and INRIA at the following URL
-# "http://www.cecill.info". 
-#
-# As a counterpart to the access to the source code and  rights to copy,
-# modify and redistribute granted by the license, users are provided only
-# with a limited warranty  and the software's author,  the holder of the
-# economic rights,  and the successive licensors  have only  limited
-# liability. 
-#
-# In this respect, the user's attention is drawn to the risks associated
-# with loading,  using,  modifying and/or developing or reproducing the
-# software by the user in light of its specific status of free software,
-# that may mean  that it is complicated to manipulate,  and  that  also
-# therefore means  that it is reserved for developers  and  experienced
-# professionals having in-depth computer knowledge. Users are therefore
-# encouraged to load and test the software's suitability as regards their
-# requirements in conditions enabling the security of their systems and/or 
-# data to be ensured and,  more generally, to use and operate it in the 
-# same conditions as regards security. 
-#
-# The fact that you are presently reading this means that you have had
-# knowledge of the CeCILL license and that you accept its terms.
-#*******************************************************************************
+##
+##  Note: this script should be used after a pass of
+##  https://wiki.eclipse.org/Development_Resources/How_to_Use_Eclipse_Copyright_Tool
+##  with the correct header (see http://www.cecill.info/placer.en.html) 
+##  with patterns (see copyright_template.txt file) and 
+##  applying back the UTF-8 encoding.
+##
 #########
 
-echo ""
+DIR=$(cd `dirname $0` && echo `git rev-parse --show-toplevel`)
 
-DATEPATTERN="2017"
-AUTHORSPATTERN="Antoine Morvan <antoine.morvan@insa-rennes.fr> (2017)
+cd $DIR
 
-TMPFILE=`mktemp --suffix=biglisttosed`
-grep "Antoine Morvan <antoine.morvan@insa-rennes.fr> (2017)
+mvn -P releng -Dtycho.mode=maven license:format
 
-echo " Starting" 
+function fixFile {
+    file=$1
 
-while read -r line
-do (
-	file="$line"
     BASENAME=$(basename "$file")
     EXTENSION="${BASENAME##*.}"
     EXTENSION=`echo $EXTENSION | tr [a-z] [A-Z]`
@@ -65,7 +36,7 @@ do (
 			#"@rem "
 			COMMENT="@rem "
 			;;
-		C |	CPP | H | JAVA | XTEND)
+		C |	CPP | H | JAVA | XTEND | BSH)
 			#" * "
 			COMMENT=" * "
 			;;
@@ -73,7 +44,7 @@ do (
 			#"# "
 			COMMENT="# "
 			;;
-		XML)
+		XML | HTML)
 			#"    "
 			COMMENT="    "
 			;;
@@ -111,12 +82,30 @@ do (
 		
     cat "$TMPFILE2" | sed -e "s/$DATEPATTERN/$GLOBDATE/g" > "$file" 
     rm $TMPFILE2
-) & done < $TMPFILE
+}
 
-sleep 2
-echo " Waiting for the threads to finish ..."
+DATEPATTERN="%%DATE%%"
+AUTHORSPATTERN="%%AUTHORS%%"
+
+TMPFILE=`mktemp --suffix=biglisttosed`
+grep "%%AUTHORS%%" -R | cut -d':' -f1 | sort -u | grep -v "copyright_template.txt" | grep -v "fix_header_copyright_and_authors.sh" | grep -v "VAADER_eclipse_preferences.epf" > $TMPFILE
+
+NBFILES=`cat $TMPFILE | wc -l`
+
+echo " Starting (#files = $NBFILES)"
+
+time (
+NBCPUS=`grep -c ^processor /proc/cpuinfo`
+((NBTHREADS=NBCPUS*2))
+while read -r line
+do
+	((i=i%NBTHREADS)); ((i++==0)) && wait && echo "    $NBFILES left ..."
+	((NBFILES=NBFILES-1))
+	fixFile "$line" &
+done < $TMPFILE
 
 wait
+)
 rm $TMPFILE
 
 echo " Done."
