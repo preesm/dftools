c38b1b554dbf8fca4de09e3ddc7f25b3be777b2e
Antoine Morvan <antoine.morvan@insa-rennes.fr>
2018-04-24 14:15:35 +0200

    [RELENG] Fix headers

--- plugins/org.ietr.dftools.algorithm/src/org/ietr/dftools/algorithm/model/sdf/visitors/DAGTransformation.java
+++ plugins/org.ietr.dftools.algorithm/src/org/ietr/dftools/algorithm/model/sdf/visitors/DAGTransformation.java
@@ -3,6 +3,7 @@
  *
  * Antoine Morvan <antoine.morvan@insa-rennes.fr> (2017 - 2018)
  * Clément Guy <clement.guy@insa-rennes.fr> (2014 - 2015)
+ * Florian Arrestier <florian.arrestier@insa-rennes.fr> (2018)
  * Jonathan Piat <jpiat@laas.fr> (2011 - 2012)
  * Karol Desnos <karol.desnos@insa-rennes.fr> (2013 - 2015)
  * Maxime Pelcat <maxime.pelcat@insa-rennes.fr> (2011)
