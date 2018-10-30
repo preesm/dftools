a4f0b17d68007b66b556da1d47631c91e9642b24
Antoine Morvan <antoine.morvan@insa-rennes.fr>
2018-10-30 09:47:41 +0100

    (Refactor)

--- plugins/org.ietr.dftools.algorithm/src/org/ietr/dftools/algorithm/exporter/Key.java
+++ plugins/org.ietr.dftools.algorithm/src/org/ietr/dftools/algorithm/exporter/Key.java
@@ -38,6 +38,8 @@
  */
 package org.ietr.dftools.algorithm.exporter;
 
+import java.util.Objects;
+
 /**
  * Class describing a GML key.
  *
@@ -152,5 +154,10 @@ public class Key {
       return kKey.applyTo.equals(this.applyTo) && kKey.name.equals(this.name) && kKey.type.equals(this.type);
     }
     return false;
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(this.applyTo, this.desc, this.id, this.name, this.type);
   }
 }
